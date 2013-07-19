96fd8ed83ea2c9ec6b9bdf26cd8998a37759825f
Guille Paz <guille87paz@gmail.com>
2013-07-19 11:58:23 -0300

    Create demo files.

---
+++ build/build.js
@@ -0,0 +1,328 @@
+
+/**
+ * Require the given path.
+ *
+ * @param {String} path
+ * @return {Object} exports
+ * @api public
+ */
+
+function require(path, parent, orig) {
+  var resolved = require.resolve(path);
+
+  // lookup failed
+  if (null == resolved) {
+    orig = orig || path;
+    parent = parent || 'root';
+    var err = new Error('Failed to require "' + orig + '" from "' + parent + '"');
+    err.path = orig;
+    err.parent = parent;
+    err.require = true;
+    throw err;
+  }
+
+  var module = require.modules[resolved];
+
+  // perform real require()
+  // by invoking the module's
+  // registered function
+  if (!module.exports) {
+    module.exports = {};
+    module.client = module.component = true;
+    module.call(this, module.exports, require.relative(resolved), module);
+  }
+
+  return module.exports;
+}
+
+/**
+ * Registered modules.
+ */
+
+require.modules = {};
+
+/**
+ * Registered aliases.
+ */
+
+require.aliases = {};
+
+/**
+ * Resolve `path`.
+ *
+ * Lookup:
+ *
+ *   - PATH/index.js
+ *   - PATH.js
+ *   - PATH
+ *
+ * @param {String} path
+ * @return {String} path or null
+ * @api private
+ */
+
+require.resolve = function(path) {
+  if (path.charAt(0) === '/') path = path.slice(1);
+
+  var paths = [
+    path,
+    path + '.js',
+    path + '.json',
+    path + '/index.js',
+    path + '/index.json'
+  ];
+
+  for (var i = 0; i < paths.length; i++) {
+    var path = paths[i];
+    if (require.modules.hasOwnProperty(path)) return path;
+    if (require.aliases.hasOwnProperty(path)) return require.aliases[path];
+  }
+};
+
+/**
+ * Normalize `path` relative to the current path.
+ *
+ * @param {String} curr
+ * @param {String} path
+ * @return {String}
+ * @api private
+ */
+
+require.normalize = function(curr, path) {
+  var segs = [];
+
+  if ('.' != path.charAt(0)) return path;
+
+  curr = curr.split('/');
+  path = path.split('/');
+
+  for (var i = 0; i < path.length; ++i) {
+    if ('..' == path[i]) {
+      curr.pop();
+    } else if ('.' != path[i] && '' != path[i]) {
+      segs.push(path[i]);
+    }
+  }
+
+  return curr.concat(segs).join('/');
+};
+
+/**
+ * Register module at `path` with callback `definition`.
+ *
+ * @param {String} path
+ * @param {Function} definition
+ * @api private
+ */
+
+require.register = function(path, definition) {
+  require.modules[path] = definition;
+};
+
+/**
+ * Alias a module definition.
+ *
+ * @param {String} from
+ * @param {String} to
+ * @api private
+ */
+
+require.alias = function(from, to) {
+  if (!require.modules.hasOwnProperty(from)) {
+    throw new Error('Failed to alias "' + from + '", it does not exist');
+  }
+  require.aliases[to] = from;
+};
+
+/**
+ * Return a require function relative to the `parent` path.
+ *
+ * @param {String} parent
+ * @return {Function}
+ * @api private
+ */
+
+require.relative = function(parent) {
+  var p = require.normalize(parent, '..');
+
+  /**
+   * lastIndexOf helper.
+   */
+
+  function lastIndexOf(arr, obj) {
+    var i = arr.length;
+    while (i--) {
+      if (arr[i] === obj) return i;
+    }
+    return -1;
+  }
+
+  /**
+   * The relative require() itself.
+   */
+
+  function localRequire(path) {
+    var resolved = localRequire.resolve(path);
+    return require(resolved, parent, path);
+  }
+
+  /**
+   * Resolve relative to the parent.
+   */
+
+  localRequire.resolve = function(path) {
+    var c = path.charAt(0);
+    if ('/' == c) return path.slice(1);
+    if ('.' == c) return require.normalize(p, path);
+
+    // resolve deps by returning
+    // the dep in the nearest "deps"
+    // directory
+    var segs = parent.split('/');
+    var i = lastIndexOf(segs, 'deps') + 1;
+    if (!i) i = 0;
+    path = segs.slice(0, i + 1).join('/') + '/deps/' + path;
+    return path;
+  };
+
+  /**
+   * Check if module is defined at `path`.
+   */
+
+  localRequire.exists = function(path) {
+    return require.modules.hasOwnProperty(localRequire.resolve(path));
+  };
+
+  return localRequire;
+};
+require.register("sticky/index.js", function(exports, require, module){
+/**
+ * Module dependencies.
+ */
+var win = window,
+    doc = win.document,
+    docEl = doc.documentElement,
+    on = win.addEventListener || win.attachEvent,
+    SCROLL = (on === win.attachEvent) ? 'onscroll' : 'scroll',
+    scrolled = false,
+    zIndex = 0,
+    requestAnimFrame = (function () {
+        return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60);
+            };
+    }());
+
+function update() {
+    // No changing, exit
+    if (!scrolled) { return; }
+
+    sticky.collection.forEach(checkPosition);
+
+    // Change status
+    scrolled = false;
+}
+
+function checkPosition(sticky) {
+    var offsetTop = sticky.el.getBoundingClientRect().top;
+
+    if (!(offsetTop >= (0 + sticky.boundary))) {
+        sticky.el.style.top = sticky.boundary + 'px';
+        sticky.el.className = 'fixed';
+
+        sticky.fixed = true;
+
+        if (sticky.fn) {
+            setTimeout(function () {
+                sticky.fn.call(sticky.el);
+            }, 0);
+        }
+    }
+
+    if (sticky.fixed && ((win.pageYOffset || docEl.scrollTop) + sticky.boundary) < sticky.originalOffsetTop) {
+        sticky.el.className = '';
+        sticky.fixed = false;
+    }
+}
+
+/**
+ * sticky factory
+ */
+function sticky(el, boundary, fn) {
+
+    sticky.collection.push(new Sticky(el, boundary, fn));
+
+    return this;
+}
+
+sticky.collection = []
+
+
+/**
+ * Sticky Class
+ */
+function Sticky(el, boundary, fn) {
+   this.init(el, boundary, fn);
+
+    return this;
+};
+
+Sticky.prototype.init = function (el, boundary, fn) {
+
+    if (el === undefined) {return;}
+
+    if (typeof boundary === 'function') {
+        fn = boundary;
+        boundary = 0;
+    }
+
+    this.el = el;
+    this.boundary = boundary || 0;
+    this.fn = fn;
+
+    this.fixed = false;
+
+    this.originalOffsetTop = this.el.getBoundingClientRect().top;
+
+    this.wrap();
+
+    return this;
+}
+
+Sticky.prototype.wrap = function () {
+    var parent = this.el.parentNode,
+        wrapper = doc.createElement('div');
+
+    wrapper.style.width = this.el.offsetWidth + 'px';
+    wrapper.style.height = this.el.offsetHeight + 'px';
+    wrapper.style.position = 'relative';
+    wrapper.style.zIndex = (zIndex += 1);
+
+    parent.insertBefore(wrapper, this.el);
+    wrapper.appendChild(this.el);
+
+    return this;
+};
+
+/**
+ * Expose Sticky
+ */
+exports = module.exports = sticky;
+
+/**
+ * Events
+ */
+on(SCROLL, function () { scrolled = true; });
+
+/**
+ * requestAnimationFrame
+ */
+(function updateloop() {
+    requestAnimFrame(updateloop);
+    update();
+}());
+});
+require.alias("sticky/index.js", "sticky/index.js");
+
