b03ecd3b05f8b7da1b66f812e7caa62f6219a4ef
Andy Burke <aburke@bitflood.org>
2013-09-17 17:00:03 -0700

    Refactor to support more options, generate some cleaner DOM elements, handle more dynamic pages, etc.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var win = window,
     on = win.addEventListener || win.attachEvent,
     SCROLL = (on === win.attachEvent) ? 'onscroll' : 'scroll',
     scrolled = false,
-    zIndex = 0,
+    zIndexOffset = 0,
     requestAnimFrame = (function () {
         return win.requestAnimationFrame ||
             win.webkitRequestAnimationFrame ||
@@ -17,130 +17,168 @@ var win = window,
             };
     }());
 
-function checkPosition(sticky) {
+var defaultOptions = {
+    zIndexBase: 0,
+    zIndexManagement: true,
+    className: 'sticky-js-fixed',
+    placeholderClassName: 'sticky-js-placeholder',
+    top: 0,
+    stuck: null,
+    unstuck: null
+}
+
+// https://github.com/gorillatron/extend
+function _extend( deep ) {
+  var out, objs, i, obj, prop, val
+
+  out = {}
+
+  typeof deep === "boolean" ? ( objs = [].slice.call(arguments, 1), deep = deep ) :
+                              ( objs = [].slice.call(arguments, 0), deep = false )
+
+  for( i = 0; i < objs.length; i++ ) {
+
+    obj = objs[ i ]
+
+    for( prop in obj ) {
+      val = obj[ prop ]
+      if( deep && typeof val === "object" && typeof out[prop] === "object") {
+        out[ prop ] = _extend( out[prop], val )
+      } else {
+        out[ prop ] = val
+      }
+      
+    }
+  }
+
+  return out
+}
+
+function checkPosition( sticky ) {
     var offsetTop = sticky.el.getBoundingClientRect().top;
 
-    if (!sticky.fixed && offsetTop <= 0) {
+    if ( !sticky.fixed && offsetTop <= sticky.options.top )
+    {
+        if ( !sticky.placeholder )
+        {
+            // insert a placeholder element into the dom so document height remains the same
+            sticky.placeholder = doc.createElement( 'div' );
+            sticky.placeholder.className = sticky.options.placeholderClassName;
+            sticky.placeholder.style.height = sticky.el.offsetHeight + 'px';
+            sticky.placeholder.style.position = 'relative';
+            sticky.el.nextElementSibling ? sticky.el.parentNode.insertBefore( sticky.placeholder, sticky.el.nextElementSibling ) : sticky.el.parentNode.appendChild( sticky.placeholder );
+        }
+        else
+        {
+            sticky.placeholder.style.display = 'block';
+        }
 
-        sticky.el.style.top = sticky.boundary + 'px';
-        sticky.el.className = sticky.el.className + ' fixed';
-
+        sticky.el.style.top = sticky.options.top + 'px';
+        sticky.el.className = sticky.el.className + ' ' + sticky.options.className;
+        if ( sticky.options.zIndexManagement )
+        {
+            sticky.el.style.zIndex = sticky.options.zIndexBase + zIndexOffset++;
+        }
         sticky.fixed = true;
 
-        if (sticky.fn) {
-            win.setTimeout(function () {
-                sticky.fn.call(sticky.el);
-            }, 0);
+        if ( sticky.options.stuck )
+        {
+            win.setTimeout( function () {
+                sticky.options.stuck.call( sticky.el );
+            }, 0 );
         }
     }
+    else if ( sticky.fixed )
+    {
+        var originalTop = sticky.el.parentElement ? sticky.el.parentElement.getBoundingClientRect().top : 0;
+        
+        if ( sticky.el.previousElementSibling )
+        {
+            originalTop = sticky.el.previousElementSibling.getBoundingClientRect().top + sticky.el.previousElementSibling.getBoundingClientRect().height;
+        }
+        
+        if ( originalTop > sticky.options.top )
+        {
+            sticky.el.style.top = sticky.originalTop;
+            sticky.el.className = sticky.el.className.replace( ' ' + sticky.options.className, '' );
+            if ( sticky.options.zIndexManagement )
+            {
+                sticky.el.style.zIndex = sticky.originalZindex;
+            }
+            sticky.fixed = false;
+            if ( sticky.placeholder )
+            {
+                sticky.placeholder.style.display = 'none';
+            }
 
-    if (sticky.fixed && ((win.pageYOffset || docEl.scrollTop) + sticky.boundary) < sticky.originalOffsetTop) {
-        sticky.el.className = sticky.el.className.replace('fixed', '');
-        sticky.fixed = false;
+            if ( sticky.options.unstuck )
+            {
+                win.setTimeout( function () {
+                    sticky.options.unstuck.call( sticky.el );
+                }, 0 );
+            }
+        }
     }
 }
 
-function update(sticky) {
-    // No changing, exit
-    if (!scrolled) { return; }
+function update( sticky ) {
+    if ( !scrolled )
+    {
+        return;
+    }
 
-    sticky.collection.forEach(checkPosition);
+    sticky.collection.forEach( checkPosition );
 
-    // Change scroll status
     scrolled = false;
 }
 
 /**
  * Sticky Class
  */
-function Sticky(el, options) {
+function Sticky( el, opts ) {
 
-    if (el === undefined) {
-        throw new Error('"sticky(el, [options])": It must receive an element.');
+    if ( el === undefined )
+    {
+        throw new Error( '"sticky( el[, options] )": No element passed.' );
     }
 
-    this.initialize(el, options);
+    this.el = el;
+    this.options = _extend( true, defaultOptions, opts );
+    this.fixed = false;
+    this.originalZindex = this.el.style.zIndex;
+    this.originalTop = this.el.style.top;
 
     return this;
 }
 
-Sticky.prototype.initialize = function (el, options) {
-
-    var num, fn;
-    options = options || {};
-
-    if (typeof options === 'number') {
-        num = options;
-        options = {
-            'boundary': num
-        };
-    }
-
-    if (typeof options === 'function') {
-        fn = options;
-        options = {
-            'fn': fn
-        };
-    }
-
-    // Config
-    this.el = el;
-    this.boundary = options.boundary || 0;
-    this.fn = options.fn;
-
-    // Defaults
-    this.fixed = false;
-    this.originalOffsetTop = this.el.getBoundingClientRect().top;
-
-    this.wrap();
-
-    return this;
-};
-
-Sticky.prototype.wrap = function () {
-    var parent = this.el.parentNode;
-
-    this.wrapper = doc.createElement('div');
-
-    this.wrapper.style.width = this.el.offsetWidth + 'px';
-    this.wrapper.style.height = this.el.offsetHeight + 'px';
-    this.wrapper.style.position = 'relative';
-    zIndex += 1;
-    this.wrapper.style.zIndex = zIndex;
-
-    parent.insertBefore(this.wrapper, this.el);
-    this.wrapper.appendChild(this.el);
-
-    return this;
-};
-
-
 Sticky.prototype.destroy = function () {
-    var parent = this.wrapper.parentNode;
-
-    this.el.className = this.el.className.replace(/fixed/, '');
+    this.el.className = this.el.className.replace( ' ' + this.options.className, '' );
     this.fixed = false;
-
-    parent.insertBefore(this.el, this.wrapper);
-    parent.removeChild(this.wrapper);
+    if ( this.placeholder )
+    {
+        this.el.parentNode.removeChild( this.placeholder );
+        this.placeholder = null;
+    }
 };
 
 /**
  * sticky factory
  */
-function sticky(nodes, options) {
+function sticky( nodes, options ) {
 
     nodes = nodes || doc.querySelectorAll('[data-sticky]');
 
-    if (nodes.length === undefined) {
-        nodes = [nodes];
+    if ( nodes.length === undefined )
+    {
+        nodes = [ nodes ];
     }
 
     var i = 0,
         len = nodes.length;
 
-    for (i; i < len; i += 1) {
-        sticky.collection.push(new Sticky(nodes[i], options));
+    for (i; i < len; i += 1)
+    {
+        sticky.collection.push( new Sticky( nodes[ i ], options ) );
     }
 
     return this;
@@ -148,38 +186,56 @@ function sticky(nodes, options) {
 
 sticky.collection = [];
 
-sticky.destroy = function (node) {
+sticky.destroy = function( node ) {
 
     var i = 0,
         len = sticky.collection.length,
         instance;
 
-    for (i; i < len; i += 1) {
-        instance = sticky.collection[i];
+    for ( i; i < len; ++i )
+    {
+        instance = sticky.collection[ i ];
 
-        if (instance.el === node) {
+        if ( instance.el === node )
+        {
             instance.destroy();
-            sticky.collection.splice(i, 1);
-            return;
+            sticky.collection.splice( i, 1 );
+            return true;
         }
-
     }
+    
+    return false;
 };
 
 /**
  * Expose Sticky
  */
-exports = module.exports = sticky;
+if ( typeof window.define === 'function' && window.define.amd !== undefined ) // AMD suppport
+{
+    window.define( 'sticky', [], function () {
+        return sticky;
+    } );
+}
+else if ( typeof module !== 'undefined' && module.exports !== undefined ) // CommonJS suppport
+{
+    module.exports = sticky;
+}
+else
+{
+    window.sticky = sticky;
+}
 
 /**
  * Events
  */
-on(SCROLL, function () { scrolled = true; });
+on( SCROLL, function () {
+    scrolled = true;
+} );
 
 /**
  * requestAnimationFrame
  */
-(function updateloop() {
-    requestAnimFrame(updateloop);
-    update(sticky);
-}());
+( function updateloop() {
+    requestAnimFrame( updateloop );
+    update( sticky );
+}() );
